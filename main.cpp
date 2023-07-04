463190abda35dbb3944d84086be71692ffd8b4ca
Bob (KorobovaAA) <alexacorobova@yandex.ru>
2023-07-05 00:52:36 +0300

    минимум

--- main.cpp
+++ main.cpp
@@ -10,5 +10,9 @@ cin >> a >> b;
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n';
+     if (a < b)
+        cout << "MIN: " << a << '\n';
+     else
+        cout << "MIN: " << b << '\n';
     return 0;
 }
